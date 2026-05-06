dcbf2465cc8a4ceda5f892a0018de37fb9e52796
Adamya Chauhan <adamya1332004@gmail.com>
2026-05-06 16:45:41 +0530

    Added create booking repo code

---
+++ src/repository/booking-repository.js
@@ -0,0 +1,28 @@
+const {Booking} = require('../models/index')
+const {ValidationError, AppError} = require('../utils/errors/index')
+const {StatusCodes} = require('http-status-codes');
+
+
+class BookingRepository {
+    async create(data) {
+        try {
+            const booking = Booking.create(data);
+            return booking;
+        } catch (error) {
+            if (error.name == 'SequelizeValidationError') {
+                throw new ValidationError(error);
+            }
+            throw new AppError('RepositoryError',
+                'Cant create Booking',
+                'There was some issue in creating a booking please try again later',
+                StatusCodes.INTERNAL_SERVER_ERROR
+            )
+        }
+    }
+
+    async update() {
+        
+    }
+}
+
+module.exports = BookingRepository;
---
+++ src/services/booking-service.js
@@ -0,0 +1,8 @@
+const BookingRepository = require('../repository/index');
+class BookingService{
+    constructor() {
+        this.bookingRepository = new BookingRepository();
+    }
+}
+
+module.exports = BookingService;
